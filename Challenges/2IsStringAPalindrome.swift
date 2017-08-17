003e9d2698b38a88708662a450a51d1296d4df05
Aivars <aivars.meijers@gmail.com>
2017-08-17 23:52:51 +0300

    Update 2IsStringAPalindrome.swift

--- Challenges/2IsStringAPalindrome.swift
+++ Challenges/2IsStringAPalindrome.swift
@@ -3,15 +3,17 @@ import Foundation
 func chalenge2 (input: String) -> Bool {
     let inputString =  input.lowercased()
     let reverseInput = String(inputString.characters.reversed())
+    print(inputString == reverseInput)
     
+    return inputString == reverseInput
     
-    if inputString == reverseInput {
-        print("true")
-        return true
-    }
-    return false
 }
-assert(chalenge2(input: "rotator") == false, "Challange 2 failed")
+
+//func challenge2(input: String) -> Bool {
+//    let lowercase = input.lowercased()
+//    return lowercase.characters.reversed() == Array(lowercase.characters)
+//}
+
+assert(challenge2(input: "rotator") == false, "Challenge 2 failed")
 assert(chalenge2(input: "Rats live on no evil star") == false, "Challange 2 failed")
 assert(chalenge2(input: "Aivars") == true, "Challange 2 failed")
-
