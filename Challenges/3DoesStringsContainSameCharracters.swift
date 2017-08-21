b08ec01ec7334c1c7ac093cee1452135c42438a8
Aivars <aivars.meijers@gmail.com>
2017-08-21 21:59:19 +0300

    Create 3DoesStringsContainSameCharracters.swift

---
+++ Challenges/3DoesStringsContainSameCharracters.swift
@@ -0,0 +1,20 @@
+//Write a function that accepts two String parameters, and returns true if they contain the same characters in any order taking into account letter case.”
+
+
+import Foundation
+
+func challenge3 (input1: String, input2: String) -> Bool {
+    var inputArray1 = Array (input1.characters).sorted()
+    var inputArray2 = Array (input2.characters).sorted()
+    
+    return inputArray1 == inputArray2
+}
+
+
+
+//tests
+assert(challenge3(input1: "abce", input2:"abce") == true, "Challenge 2 failed")
+assert(challenge3(input1: "abc", input2:"cba") == true, "Challenge 2 failed")
+assert(challenge3(input1: "a 1 b2", input2:"b 1 a 2") == false, "Challenge 2 failed")
+assert(challenge3(input1: "abce", input2:"abca") == false, "Challenge 2 failed")
+assert(challenge3(input1: "abc", input2:"cbAa") == false, "Challenge 2 failed")
